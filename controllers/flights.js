e6fa5bed468d1e0ae4d93e596fea621d49304404
Seun Dada <seun.dada777@gmail.com>
2024-04-16 09:25:44 -0400

    add html function and route for show edit and index

--- controllers/flights.js
+++ controllers/flights.js
@@ -8,14 +8,14 @@ function newFlight(req, res) {
 
 function create(req, res) {
   // remove empty properties on req.body
-  for (let key in req.body) {
-    if (req.body[key] === '') delete req.body[key]
+  for (let number in req.body) {
+    if (req.body[number] === '') delete req.body[number]
   }
-  // use Movie model to create movie
+  // use flight model to create flight
   Flight.create(req.body)
   .then(flight => {
     // redirect somewhere
-    res.redirect('/flight')
+    res.redirect('/flights')
   })
   .catch(err => {
     console.log(err)
@@ -23,7 +23,67 @@ function create(req, res) {
   })
 }
 
+function index(req, res) {
+  // use flight model to search for all flights
+  Flight.find({})
+  .then(flights => {
+    // render a view (flights/index) and pass flights and title 
+    res.render('flights/index', {
+      flights: flights,
+      title: 'All flights'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function show(req, res) {
+  Flight.findById(req.params.flightId)
+  .then(flight => {
+    res.render('flights/show', {
+      flight: flight,
+      title: 'Flight Detail'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function deleteFlight(req, res) {
+  Flight.findByIdAndDelete(req.params.flightId)
+  .then(flight => {
+    // redirect back to index view
+    res.redirect('/flights')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+function edit(req, res) {
+  Flight.findById(req.params.flightId)
+  .then(flight => {
+    res.render('flights/edit', {
+      flight: flight,
+      title: 'Edit Flight'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights')
+  })
+}
+
 export {
   newFlight as new,
   create,
+  index,
+  show,
+  deleteFlight as delete,
+  edit,
 }
--- routes/flights.js
+++ routes/flights.js
@@ -3,13 +3,22 @@ import * as flightsCtrl from '../controllers/flights.js'
 
 const router = Router()
 
-// // GET localhost:3000/flights
-// router.get('/', flightsCtrl.index)
+// GET localhost:3000/flights
+router.get('/', flightsCtrl.index)
 
 // GET localhost:3000/flights/new
 router.get('/new', flightsCtrl.new)
 
-// GET localhost:3000/flights/new
-router.get('/', flightsCtrl.create)
+// GET localhost:3000/flights/:flightId
+router.get('/:flightId', flightsCtrl.show)
+
+// GET localhost:3000/flights/:flightId/edit
+router.get('/:flightId/edit', flightsCtrl.edit)
+
+// GET localhost:3000/flights
+router.post('/', flightsCtrl.create)
+
+// DELETE localhost:3000/flights/:flightId
+router.delete('/:flightId', flightsCtrl.delete)
 
 export { router }
