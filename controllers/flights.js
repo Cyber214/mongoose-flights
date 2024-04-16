59caa26e0778188e5bb50b0379d18853d1372ada
Seun Dada <seun.dada777@gmail.com>
2024-04-15 23:36:06 -0400

    add create function and route

--- controllers/flights.js
+++ controllers/flights.js
@@ -6,6 +6,24 @@ function newFlight(req, res) {
   })
 }
 
+function create(req, res) {
+  // remove empty properties on req.body
+  for (let key in req.body) {
+    if (req.body[key] === '') delete req.body[key]
+  }
+  // use Movie model to create movie
+  Flight.create(req.body)
+  .then(flight => {
+    // redirect somewhere
+    res.redirect('/flight')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights/new')
+  })
+}
+
 export {
   newFlight as new,
+  create,
 }
--- routes/flights.js
+++ routes/flights.js
@@ -3,8 +3,13 @@ import * as flightsCtrl from '../controllers/flights.js'
 
 const router = Router()
 
+// // GET localhost:3000/flights
+// router.get('/', flightsCtrl.index)
+
 // GET localhost:3000/flights/new
 router.get('/new', flightsCtrl.new)
 
+// GET localhost:3000/flights/new
+router.get('/', flightsCtrl.create)
 
 export { router }
